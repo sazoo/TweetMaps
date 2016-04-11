568e5cd4d61d45cbd57cbecc1ddbedcc3eeb3519
Deo Al Emboltorio <daae25@gmail.com>
2016-04-11 21:18:30 +0800

    added error handling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use Log;
 use Redis;
 use App\Tweet;
 use App\RawTweet;
@@ -30,23 +31,27 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
 			while(Redis::llen('tweets') > 0){
-				$raw = Redis::lpop('tweets');
-				$tweetArr = json_decode($raw, true);
-				
-				$tweet = new Tweet;
-				$tweet->tweet_id = $tweetArr['id'];
-				$tweet->date_created = $tweetArr['created_at'];
-				$tweet->coordinates = $tweetArr['coordinates'];
-				$tweet->user_id = $tweetArr['user']['id'];
-				$tweet->tweet = $tweetArr['text'];
-				$tweet->screen_name = $tweetArr['user']['screen_name'];
-				
-				$tweet->save();
-				
-				$rawTweet = new RawTweet;
-				$rawTweet->raw = $raw;
-				
-				$rawTweet->save();
+                try{
+                    $raw = Redis::lpop('tweets');
+                    $tweetArr = json_decode($raw, true);
+
+                    $tweet = new Tweet;
+                    $tweet->tweet_id = $tweetArr['id'];
+                    $tweet->date_created = $tweetArr['created_at'];
+                    $tweet->coordinates = $tweetArr['coordinates'];
+                    $tweet->user_id = $tweetArr['user']['id'];
+                    $tweet->tweet = $tweetArr['text'];
+                    $tweet->screen_name = $tweetArr['user']['screen_name'];
+
+                    $tweet->save();
+
+                    $rawTweet = new RawTweet;
+                    $rawTweet->raw = $raw;
+
+                    $rawTweet->save();
+                }catch(Exception $e) {
+                    Log::error($e->getMessage());
+                }
 			}
 		})->everyMinute();
     }
