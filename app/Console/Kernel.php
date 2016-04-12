09258a313aa74d60e21d5dae49ee90b246e67a96
Deo Al Emboltorio <daae25@gmail.com>
2016-04-12 19:16:04 +0800

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends ConsoleKernel
                     $tweet = new Tweet;
                     $tweet->tweet_id = $tweetArr['id'];
                     $tweet->date_created = $tweetArr['created_at'];
-                    if(isset($tweetArr['coordinates'])){
+                    if(isset($tweetArr->{'coordinates'})){
                         $tweet->longitude = $tweetArr['coordinates']['coordinates'][0];
                         $tweet->latitude = $tweetArr['coordinates']['coordinates'][1];
                     }
