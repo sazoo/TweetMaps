81fabf3f4412c9c912713d568d61c36564b18457
Deo Al Emboltorio <daae25@gmail.com>
2016-04-13 23:34:41 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,9 +11,9 @@ use App\Tweet;
 class HomeController extends Controller
 {
     public function getLatestTweets(){
-        $count = \DB::table('tweets')->where(\DB::raw('created_at >= DATE_SUB(NOW(), INTERVAL 1 HOUR)'))->count();
-        var_dump($count);
-        $tweets = Tweet::orderBy('created_at', 'DESC')->take($count)->get();
+        $tweets = Tweet::where('created_at >= DATE_SUB(NOW(), INTERVAL 1 HOUR)')->count();
+        //var_dump($count);
+        //$tweets = Tweet::orderBy('created_at', 'DESC')->take($count)->get();
         return response()->json($tweets);
     }
 }
