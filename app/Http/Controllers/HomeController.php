dd220e589f58a80801f428394e2de5312f6cade3
Deo Al Emboltorio <daae25@gmail.com>
2016-04-13 22:59:22 +0800

    added getlatesttweets endpoint

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Tweet;
+
+class HomeController extends Controller
+{
+    public function getLatestTweets(){
+        $tweets = \DB::table('tweets') ->where(\DB::raw('date >= DATE_SUB(NOW(), INTERVAL 1 HOUR)'));
+        return json($tweets);
+    }
+}
