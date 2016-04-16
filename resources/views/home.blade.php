f7eedc83ef9b286e16d57c9b82626e33d0c18772
Deo Al Emboltorio <daae25@gmail.com>
2016-04-17 00:18:57 +0800

    hay

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,10 @@
             width: 100%;
             height: 100%;
         }
-        
+        #map {
+            width: 100%;
+            height: 100%;
+        }
     </style>
 
     <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
